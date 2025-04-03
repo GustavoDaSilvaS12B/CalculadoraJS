7b8359531962c28df8c473fde99bdeec548e977e
gs9812125 <gs9812125@gmail.com>
2025-04-03 17:13:31 +0000

    criação da lógica para que seja possível selecionar apenas um botão de porecentagem de gorjeta

--- main.js
+++ main.js
@@ -23,10 +23,16 @@ function receiveNumberOfPeopleValue(){
 function receiveTipPercentageValue(value){
     tipPercentage =  value/100
 
-    
+    removeClassButtonSelected()
 
-    buttonSelected = document.querySelector("")
+    buttonSelected = document.querySelector('#button-${value}')
+    buttonSelected.classList.add("button-selected")
+}
 
+function removeClassButtonSelected() {
+  if(buttonSelected !== null)
+    buttonSelected.classlist.remove("button-selected")
+    buttonSelected = null
 }
 
 function calculate (){
