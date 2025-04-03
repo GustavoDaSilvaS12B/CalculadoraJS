666cf9f166710d99d8305e1a015a03da865027ca
gs9812125 <gs9812125@gmail.com>
2025-04-03 16:16:43 +0000

    Funções para receber o valor da conta e a quantidade de pessoas no JS

---
+++ main.js
@@ -0,0 +1,26 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+
+let billInput = document.querySelector("#bill")
+bill.Input.addEventListener("input, receiveBillValue")
+
+let numberOfPeopleInput = document.querySelector("#people")
+numberOfPeopleInput.addEventListener("#input", receiveNumberOfPeopleValue)
+
+
+function receiveBillValue(){
+    bill = billInput.valueAsNumber
+
+    calculate()
+}
+
+function receiveNumberOfPeopleValue(){
+      numberOfPeople = numberOfPeopleInput.valueAsNumber
+}
+
+function calculate (){
+  if (bill !== 0  && tipPercentage !== 0 && numberOfPeople !== 0){
+    console.log ("É possível calcular")
+  }
+}
